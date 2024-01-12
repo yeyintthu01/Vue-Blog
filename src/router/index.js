2704cfa89b7934c7171ef3253c6ccbfc6e48b8ea
Ye Yint Thu <thuyeyint018@gmail.com>
2024-01-12 12:40:58 +0630

    post detail page and getPost composible

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Detail from '../views/Detail.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/posts/:id',
+    name: 'Detail',
+    component: Detail,
+    props: true
   }
 ]
 
---
+++ src/views/Detail.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-if="post">
+        <h2>{{post.title}}</h2>
+        <p>{{post.body}}</p>
+  </div>
+  <div v-else>
+    Loading...
+  </div>
+</template>
+
+<script>
+import getPost from '../composables/getPost'
+export default {
+    props:['id'],
+    setup(props){
+        let {post,error,load} = getPost(props.id)
+
+        load()
+        return {post,error}
+    }
+}
+</script>
+
+<style>
+
+</style>
