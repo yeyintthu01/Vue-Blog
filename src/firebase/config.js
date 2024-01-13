7724dfa44d3410f91a527458317675b03a673b53
Ye Yint Thu <thuyeyint018@gmail.com>
2024-01-13 17:23:55 +0630

    connecting our project to firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/firestore"
+const firebaseConfig = {
+    apiKey: "AIzaSyD2jSru0cY-e9HQcLReL5J6T42DbGJet7c",
+    authDomain: "vue-blog-22479.firebaseapp.com",
+    projectId: "vue-blog-22479",
+    storageBucket: "vue-blog-22479.appspot.com",
+    messagingSenderId: "585360732847",
+    appId: "1:585360732847:web:3602bd7a4d834781be1a5a"
+  };
+
+  firebase.initializeApp(firebaseConfig)
+
+  let db=firebase.firestore()
+
+  export {db}
