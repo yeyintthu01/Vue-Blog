6a71a1f1dffb0d929f21083e19164be19d761692
Ye Yint Thu <thuyeyint018@gmail.com>
2024-01-14 19:20:16 +0630

    fetch single post from firebase

--- src/composables/getPost.js
+++ src/composables/getPost.js
@@ -1,19 +1,13 @@
 import { ref } from "vue"
+import { db } from "../firebase/config"
 
 let getPost=(id)=>{
     let post=ref(null)
     let error=ref("")
     let load=async()=>{
         try{
-            // await new Promise((resolve,reject)=>{
-            //     setTimeout(resolve,2000)
-            // })
-            let response=await fetch("http://localhost:3000/posts/"+id)
-            if(response.status===404){
-                throw new Error("Not Found Url")
-            }
-            let data=await response.json()
-            post.value=data
+            let doc=await db.collection("posts").doc(id).get()
+                post.value={id:doc.id,...doc.data()}
         }catch(err){
             error.value=err
         }
